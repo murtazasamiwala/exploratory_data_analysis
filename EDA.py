0449c900c5c3027d2b9707d0b86d5f8323f88479
murtazasamiwala <murtazasamiwala@gmail.com>
2019-09-06 00:42:17 +0530

    added Chi squared test of independence

--- EDA.py
+++ EDA.py
@@ -144,5 +144,44 @@ def null_check(df,drop_thresh=90):
 
 
 
+class ChiSquare:
+    def __init__(self, dataframe):
+        self.df = dataframe
+        self.p = None #P-Value
+        self.chi2 = None #Chi Test Statistic
+        self.dof = None
+        
+        self.dfObserved = None
+        self.dfExpected = None
+        self.drop_list=[]
+        
+    def _print_chisquare_result(self, colX, alpha):
+        result = ""
+        if self.p<alpha:
+            result="{0} is IMPORTANT for Prediction".format(colX)
+        else:
+            result="{0} is NOT an important predictor. (Discard {0} from model)".format(colX)
+            self.drop_list.append(colX)
+
+        print(result)
+        
+    def TestIndependence(self,colX,colY, alpha=0.05):
+        X = self.df[colX].astype(str)
+        Y = self.df[colY].astype(str)
+        
+        self.dfObserved = pd.crosstab(Y,X) 
+        chi2, p, dof, expected = stats.chi2_contingency(self.dfObserved.values)
+        self.p = p
+        self.chi2 = chi2
+        self.dof = dof 
+        
+        self.dfExpected = pd.DataFrame(expected, columns=self.dfObserved.columns, index = self.dfObserved.index)
+        
+        self._print_chisquare_result(colX,alpha)
+    
+    def Testall(self,colY):
+        for i in self.df.columns:
+            self.TestIndependence(i,colY)
+        return self.drop_list
 
 
